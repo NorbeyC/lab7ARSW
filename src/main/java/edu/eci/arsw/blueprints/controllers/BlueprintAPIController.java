686c5358d342e57bd59d254dc48f2a259ea46c14
Diego Leon <diego.leon-ba@mail.escuelaing.edu.co>
2022-09-27 18:04:46 -0500

    Actualizacion proyecto

---
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.blueprints.controllers;
+
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.services.BlueprintsServices;
+
+/**
+ *
+ * @author hcadavid
+ */
+@RestController
+public class BlueprintAPIController {
+
+    @Autowired
+    BlueprintsServices service;
+
+    @RequestMapping(value = "/blueprints", method = RequestMethod.GET)
+    public ResponseEntity<?> controllerGetBlueprints(){
+        Set<Blueprint> blueprints = null;
+        try {
+            blueprints = service.getAllBlueprints();
+            service.applyFilter(blueprints);
+            return new ResponseEntity<>(blueprints,HttpStatus.ACCEPTED);
+        } catch (Exception ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error"+ex.getMessage(),HttpStatus.NOT_FOUND);
+        }    
+    }
+
+    @RequestMapping(value = "/blueprints/{author}", method = RequestMethod.GET)
+    public ResponseEntity<?> controllerGetBluePrintsByAuthor(@PathVariable String author){
+        Set<Blueprint> blueprints = null;
+        try {
+            blueprints = service.getBlueprintsByAuthor(author);
+            service.applyFilterByAuthor(blueprints, author);
+            return new ResponseEntity<>(blueprints,HttpStatus.ACCEPTED);
+        } catch (Exception ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error"+ex.getMessage(),HttpStatus.NOT_FOUND);
+        } 
+    }
+
+    @RequestMapping(value = "/blueprints/{author}/{bpname}", method = RequestMethod.GET)
+    public ResponseEntity<?> controllerGetBluePrintsByName(@PathVariable String author, @PathVariable String bpname){
+        Blueprint blueprint = null;
+        try {
+            blueprint = service.getBlueprint(author, bpname);
+            service.applyFilter(blueprint);
+            return new ResponseEntity<>(blueprint,HttpStatus.ACCEPTED);
+        } catch (Exception ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error"+ex.getMessage(),HttpStatus.NOT_FOUND);
+        } 
+    }
+
+    @RequestMapping(value = "/blueprints/addBlueprint",method = RequestMethod.POST)
+    public ResponseEntity<?> controllerNewBluePrint(@RequestBody Blueprint bp){
+        try {
+            service.addNewBlueprint(bp);
+            return new ResponseEntity<>("Se ha agregado Correctamente",HttpStatus.CREATED);
+        } catch (Exception e) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, e);
+            return new ResponseEntity<>("Error"+e.getMessage(),HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    @PutMapping(value = "/blueprints/{author}/{bpname}")
+    public ResponseEntity<?> controllerPutBluePrint(@PathVariable("author") String author, @PathVariable("bpname") String bpname, @RequestBody Blueprint bp){
+        try {
+            service.updateBlueprint(author, bpname, bp.getPoints());
+            return new ResponseEntity<>(service.getBlueprint(author, bpname),HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, e);
+            return new ResponseEntity<>("Error"+e.getMessage(),HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+}
+
---
+++ src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.blueprints.persistence.impl;
+
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.model.Point;
+import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
+import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
+import edu.eci.arsw.blueprints.persistence.BlueprintsPersistence;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * @author hcadavid
+ */
+@Component
+@Qualifier("Memory")
+public class InMemoryBlueprintPersistence implements BlueprintsPersistence{
+
+    private final Map<Tuple<String,String>,Blueprint> blueprints=new HashMap<>();
+
+    public InMemoryBlueprintPersistence() {
+        //load stub data
+        Point[] pts=new Point[]{new Point(140, 140),new Point(115, 115)};
+        Blueprint bp=new Blueprint("_authorname_", "_bpname_ ",pts);
+        blueprints.put(new Tuple<>(bp.getAuthor(),bp.getName()), bp);
+        for(int i=0;i<2;i++){
+            int p1 = (int) (Math.random()*100);
+            int p2 = (int) (Math.random()*100);
+            Point[] pt=new Point[]{new Point(p1, p1),new Point(p2, p2)};
+            bp = new Blueprint("diego",("plano"+(i+1)),pt);
+            blueprints.put(new Tuple<>(bp.getAuthor(),bp.getName()), bp);
+        }
+        bp = new Blueprint("norbey","plano3",pts);
+        blueprints.put(new Tuple<>(bp.getAuthor(),bp.getName()), bp);        
+    } 
+        
+    
+
+    @Override
+    public void saveBlueprint(Blueprint bp) throws BlueprintPersistenceException {
+        if (this.blueprints.containsKey(new Tuple<>(bp.getAuthor(),bp.getName()))){
+            throw new BlueprintPersistenceException("The given blueprint already exists: "+bp);
+        }
+        else{
+            this.blueprints.put(new Tuple<>(bp.getAuthor(),bp.getName()), bp);
+        }        
+    }
+
+    @Override
+    public Blueprint getBlueprint(String author, String bprintname) throws BlueprintNotFoundException {
+        System.out.println(blueprints.size());
+        return blueprints.get(new Tuple<>(author, bprintname));
+    }
+
+    @Override
+    public Set<Blueprint> getBluePrints() throws BlueprintPersistenceException, BlueprintNotFoundException {
+        Set<Blueprint> bprints = new HashSet<>();
+        for(Tuple<String,String> tuple: this.blueprints.keySet()){
+                bprints.add(blueprints.get(tuple));
+        }
+        return bprints;
+    }
+
+    @Override
+    public Set<Blueprint> getBlueprintsByAuthor(String author) throws BlueprintNotFoundException {
+        Set<Blueprint> bprints = new HashSet<>();
+        for(Tuple<String,String> tuple: this.blueprints.keySet()){
+            if(tuple.o1.equals(author)){
+                bprints.add(blueprints.get(tuple));
+            }
+        }
+        return bprints;
+    }
+}
